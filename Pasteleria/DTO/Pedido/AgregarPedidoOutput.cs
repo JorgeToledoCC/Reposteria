eb0c0cf6b1b0aae9c8cf791a3fbebbff25585505
Jorge Toledo <tu-correo-de-github@ejemplo.com>
2026-04-30 14:13:20 -0400

    AVANCE PROYECT0 FINAL

--- Pasteleria/DTO/Pedido/AgregarPedidoOutput.cs
+++
@@ -1,8 +0,0 @@
-namespace Pasteleria.DTO.Pedido;
-public class AgregarPedidoOutput()
-{
-    public DateTime FechaEntrega {get;set;}
-    public decimal Total {get;set;}
-    public List<ProductosInput> Productos{get;set;} = new List<ProductosInput>();
-}
-
